9880d9f18233916689f036c21a039964c0a3b04d
Cornelis Etta <cornelis.etta1@gmail.com>
2023-03-19 13:33:42 -0700

    c-Hello,World

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * */
+int main(void)
+{
+        prinf("Size of a char: %d byte(s)\n", sizeof(char));
+	prinf("Size of a int: %d byte(s)\n", sizeof(int));
+	prinf("Size of a long: %d byte(s)\n", sizeof(long));
+	prinf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+        prinf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+~                     
